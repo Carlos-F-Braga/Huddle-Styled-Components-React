ca6c4db3a50c5035b7d5aef604bf7775921086ac
Carlos <carlos.goncalves@agrofauna.com.br>
2022-04-14 11:13:06 -0300

    Card Component Content Programmed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { ThemeProvider } from "styled-components";
 import Header from "./components/Header";
+import Card from "./components/Card";
 import { Container } from "./components/styles/Container.styled";
 import GlobalStyles from "./components/styles/Global";
+import content from "./content";
 
 const theme = {
   colors: {
@@ -19,7 +21,9 @@ function App() {
     <GlobalStyles />
     <Header/>
     <Container>
-        <h1>Hello World</h1>
+        {content.map((item, index) => (
+          <Card keu={index} item={item}/>
+        ))}
     </Container>
     </>
     </ThemeProvider>
---
+++ src/content.js
@@ -0,0 +1,22 @@
+const content = [
+    {
+      id: 1,
+      title: 'Cresça Junto',
+      body: 'Produza discussões importantes com sua andiência e construa uma comunidade forte e leal. Pense em todas as conversas perspicazes que voce perderia utilizando um formulário de feedback convencional.',
+      image: 'illustration-grow-together.svg',
+    },
+    {
+      id: 2,
+      title: 'Conversas Fluentes',
+      body: "Voce não paginaria uma conversa na vida real, então por que fazer isso online? Nossas threads tem carregamento just-in-time para uma fluência muito mais natural.",
+      image: 'illustration-flowing-conversation.svg',
+    },
+    {
+      id: 3,
+      title: 'Seus Usuários',
+      body: "Não leva tempo nenhum para integrar o Huddle com a solução de autenticação que seu app utiliza. Isso significa que, uma vez que voce tenha logado em seu app, seus usuários podem começar a conversar imediatamente",
+      image: 'illustration-your-users.svg',
+    },
+  ]
+  
+  export default content
