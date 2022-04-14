63e03c212d39109961a2afa69570efc334ab8dbf
Carlos <carlos.goncalves@agrofauna.com.br>
2022-04-14 10:55:58 -0300

    Header Core components Programmed and Functional

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
-import { StyledHeader, Nav, Logo } from "./styles/Header.styled"
+import { StyledHeader, Nav, Logo, Image } from "./styles/Header.styled"
 import { Container } from "./styles/Container.styled"
+import { Flex } from "./styles/Flex.styled"
 import { Button } from "./styles/Button.styled"
 
 export default function Header () {
@@ -10,6 +11,27 @@ export default function Header () {
                 <Logo src='./images/logo.svg' alt='' />
                 <Button>Teste de Graça</Button>
             </Nav>
+            <Flex>
+                <div>
+                    <h1>
+                        Construa a Comunidade que seus Fãs iram Amar!
+                    </h1>
+
+                    <p>
+                        Huddle reimagina a forma que nós construímos comunidades. Voce tem uma voz,
+                        mas também sua audiência possui uma. Crie conexões com seus usuários conforme
+                        voce se envolve em uma discussão genuína.
+                    </p>
+
+                    <Button bg='#ff0099' color='#fff'>
+                        Comece a Utilizar de Graça
+                    </Button>
+                </div>
+
+                <Image src='./images/illustration-mockups.svg' alt=''>
+
+                </Image>
+            </Flex>
             </Container>
         </StyledHeader>
     )
